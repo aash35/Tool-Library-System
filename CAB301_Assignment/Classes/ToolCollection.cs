7ca7f7cfbc32ba571c609e7f00dc48b91422b2d1
Adrian Ash <adrianash2705@gmail.com>
2021-04-24 01:30:34 +1000

    added the interfaces to there respective classes

--- CAB301_Assignment/Classes/ToolCollection.cs
+++ CAB301_Assignment/Classes/ToolCollection.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace Assignment.Classes
 {
-    class ToolCollection
+    class ToolCollection : iToolCollection
     {
+        public int Number => throw new NotImplementedException();
+
+        public void add(iTool aTool)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void delete(iTool aTool)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool search(iTool aTool)
+        {
+            throw new NotImplementedException();
+        }
+
+        public iTool[] toArray()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- CAB301_Assignment/Classes/ToolLibrarySystem.cs
+++ CAB301_Assignment/Classes/ToolLibrarySystem.cs
@@ -6,7 +6,66 @@ using System.Threading.Tasks;
 
 namespace Assignment.Classes
 {
-    class ToolLibrarySystem
+    class ToolLibrarySystem : iToolLibrarySystem
     {
+        public void add(iTool aTool)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void add(iTool aTool, int quantity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void add(iMember aMember)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void borrowTool(iMember aMember, iTool aTool)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void delete(iTool aTool)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void delete(iTool aTool, int quantity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void delete(iMember aMember)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void displayBorrowingTools(iMember aMember)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void displayTools(string aToolType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void displayTopTHree()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string[] listTools(iMember aMember)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void returnTool(iMember aMember, iTool aTool)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
